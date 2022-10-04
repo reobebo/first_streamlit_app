1540a24ea4e3f0dfd6af61d715e1cc1e15dd7f12
Rakip Bebo <reobebo1@hotmail.com>
2022-10-04 10:46:30 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -44,6 +44,8 @@ streamlit.dataframe(my_data_rows)
 add_my_fruit = streamlit.text_input('What fruit would you like to add?','Kiwi')
 streamlit.write('Thanks for adding ', fruit_choice)
 
+my_cur.execute("insert into fruit_load_list values ('from streamlit')")
+
 
 
 

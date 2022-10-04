4b7a1e48f08ac425fc34b115d643f7e29a8553a7
Rakip Bebo <reobebo1@hotmail.com>
2022-10-03 20:58:55 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,7 +3,7 @@ import pandas as pd
 import requests
 streamlit.header('Fruityvice Fruit Advice!')
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-streamlit.text(fruityvice_response)
+streamlit.text(fruityvice_response.json())
 
 streamlit.title('My Parents New Healthy Diner')
 
